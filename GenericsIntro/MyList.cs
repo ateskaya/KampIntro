e88b82ae8196f39b8488674a45dfda3e233f6d33
ibrahim ensar <ibrahim.ensar@ogr.dpu.edu.tr>
2023-05-18 19:44:11 +0300

    18 mayıs 2023

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        T[] list;
+        //constructor
+        public MyList()
+        {
+            list = new T[0];
+        }
+        public void Add(T item) 
+        {
+            T[] tempArray = list;
+            list = new T[list.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                list[i] = tempArray[i];
+            }
+            list[list.Length-1] = item;
+        }
+    }
+}
---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //string[] isimler = new string[] { "İbrahim", "Bayram", "Kamil", "Ramil" };
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //Console.WriteLine(isimler[3]);
+
+            //string[] tutucu = new string[] { };
+            //tutucu = isimler;
+            //isimler = new string[5];
+
+
+            List<string> isimler2 = new List<string> { "İbrahim", "Bayram", "Kamil", "Ramil" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("Fidan");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+
+
+        }
+    }
+}
